6cc36939117815ebf232bfac4681d39638df8843
zhenyuss <44225415+zhenyuss@users.noreply.github.com>
2018-10-30 10:47:08 +0800

    Update map_event.js

--- map_event.js
+++ map_event.js
@@ -5,7 +5,7 @@ var web3;
 if (typeof web3 !== 'undefined') {
     web3 = new Web3(web3.currentProvider);
 } else {
-    web3 = new Web3(new Web3.providers.WebsocketProvider("http://127.0.0.1:8546"));
+    web3 = new Web3(new Web3.providers.WebsocketProvider("ws://127.0.0.1:8546"));
 }
         
 var contractAbi = [
